1a77f73b15115caf76c9b3d523bfe2ebdfda2b15
theKyrylo <zhulin.pers0n4l@gmail.com>
2024-02-13 12:40:22 +0100

    Created simple Telegram bot using Telegram.Bot lib

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Telegram.Bot;
+string TOKEN = File.ReadAllText(@".\TOKEN.txt");
+        
+var botClient = new TelegramBotClient(TOKEN);
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
